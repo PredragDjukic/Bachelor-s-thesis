b15b94f7ff73ce8daf209b45dcda14a31621745a
djukic.predrag <predrag.djukic@marbleit.rs>
2022-03-25 00:35:15 +0100

    Create 002 script with package and bundle table

--- Database/000.002.sql
+++ Database/000.002.sql
@@ -1,5 +1,22 @@
 
 create table Package
 (
-	
+	Id int primary key identity,
+	Name nvarchar(30) not null,
+	NumberOfSession int not null,
+	Price decimal(12, 2) not null,
+	TrainerId int not null,
+	CreatedAt Date not null,
+	UpdatedAt Date not null
+)
+--#ON DELTE NO ACTION (Case if trainer deletes his package, but exercisers still have active bundles)
+create table Bundle
+(
+	Id int primary key identity,
+	SessionsLeft int not null,
+	PackageId int not null,
+	ExerciserId int not null,
+	IsActive bit not null,
+	CreatedAt Date not null,
+	UpdatedAt Date not null
 )
