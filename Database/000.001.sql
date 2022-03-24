67d3fada80a0f3c0da2d0d1c1a9c0a3680293e1b
djukic.predrag <predrag.djukic@marbleit.rs>
2022-03-25 00:04:57 +0100

    Select right user implementation in db

--- Database/000.001.sql
+++ Database/000.001.sql
@@ -1,7 +1,34 @@
 print('---DROPING TABLES---')
+drop table if exists [User]
 drop table if exists [Trainer]
 drop table if exists [Exerciser]
-drop table if exists [User]
+go
+
+print('---CREATE TRAINER TABLE---')
+create table [Trainer]
+(
+	Id int not null primary key identity,
+	Bio nvarchar(1000) not null,
+	Experience int not null,
+	UserId int not null
+);
+
+alter table [Trainer] add constraint UC_Trainer_UserId unique (UserId)
+go
+
+print('---CREATE EXERCISER TABLE---')
+create table [Exerciser]
+(
+	Id int not null primary key identity,
+	ExerciseHistory int not null,
+	Goal int not null,
+	MessageForCoaches nvarchar(1000) not null,
+	EmergencyContactFullName nvarchar(100) not null,
+	EmergencyContactPhoneNumber nvarchar(20) not null,
+	UserId int not null
+);
+
+alter table [Exerciser] add constraint UC_Exerciser_UserId unique (UserId)
 go
 
 print('---CREATE USER TABLE---')
@@ -25,35 +52,18 @@ create table [User]
 	DateOfBirth Date not null,
 	ProfilePhotoUrl nvarchar(255) not null,
 	CreatedAt Date not null,
-	UpdatedAt Date not null
+	UpdatedAt Date not null,
+	TrainerId int null,
+	ExerciserId int null
 )
 
 alter table [User] add constraint UC_User_Email unique (Email)
 alter table [User] add constraint UC_User_PhoneNumber unique (PhoneNumber)
 
-print('---CREATE TRAINER TABLE---')
-create table [Trainer]
-(
-	Bio nvarchar(1000) not null,
-	Experience int not null,
-	UserId int not null
-);
+alter table [User] add constraint FK_User_Trainer Foreign Key (TrainerId) references [Trainer](Id) on delete cascade;
+alter table [User] add constraint FK_User_Exerciser Foreign Key (ExerciserId) references [Exerciser](Id) on delete cascade;
 
-alter table [Trainer] add constraint FK_Trainer_User Foreign Key (UserId) references [User](Id) on delete cascade;
-alter table [Trainer] add constraint UC_Trainer_UserId unique (UserId)
-go
+alter table [User] add constraint UC_User_TrainerId unique (TrainerId)
+alter table [User] add constraint UC_User_ExerciserId unique (ExerciserId)
 
-print('---CREATE EXERCISER TABLE---')
-create table [Exerciser]
-(
-	ExerciseHistory int not null,
-	Goal int not null,
-	MessageForCoaches nvarchar(1000) not null,
-	EmergencyContactFullName nvarchar(100) not null,
-	EmergencyContactPhoneNumber nvarchar(20) not null,
-	UserId int not null
-);
-
-alter table [Exerciser] add constraint FK_Exerciser_User Foreign Key (UserId) references [User](Id) on delete cascade;
-alter table [Exerciser] add constraint UC_Exerciser_UserId unique (UserId)
-go
+alter table [User] add constraint CHK_User_Role check ((TrainerId is null AND ExerciserId is not null) OR (TrainerId is not null AND ExerciserId is null))
