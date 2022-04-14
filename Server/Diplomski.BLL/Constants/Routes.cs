9a8d937547f788f88e4e111f8cb7d389578029f7
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-15 00:09:12 +0200

    Add Routes constants

---
+++ Server/Diplomski.BLL/Constants/Routes.cs
@@ -0,0 +1,10 @@
+﻿namespace Diplomski.BLL.Constants
+{
+    public static class Routes
+    {
+        private const string Base = "api/";
+
+        //Auth
+        public const string Auth = Base + "auth/";
+    }
+}
