4b7e538b6ad3ae7113b076bd307b0f64c7083ce6
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-15 00:08:00 +0200

    Add auth and base controller

---
+++ Server/Diplomski.BLL/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Diplomski.BLL.Constants;
+using Diplomski.BLL.DTOs;
+using Diplomski.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diplomski.BLL.Controllers
+{
+    [ApiController]
+    [Route(Routes.Auth)]
+    public class AuthController : BaseController
+    {
+        private readonly IUserService _service;
+
+
+        public AuthController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public ActionResult Register([FromBody] UserRegisterDto dto)
+        {
+            string token = _service.Register(dto);
+
+            return Ok(new { Token = token });
+        }
+    }
+}
---
+++ Server/Diplomski.BLL/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Diplomski.BLL.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected int CurrentUserId {
+            get => Convert.ToInt32(HttpContext?.Items["Id"]);
+        }
+    }
+}
