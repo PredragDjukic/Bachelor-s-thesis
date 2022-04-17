c631ad38ce2f9940145167d24dff91d28e147dfb
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-17 10:59:36 +0200

    Create authController and implement register EP

---
+++ Server/Diplomski.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Diplomski.BLL.DTOs;
+using Diplomski.BLL.Interfaces;
+using Diplomski.BLL.Utils.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diplomski.Presentation.Controllers
+{
+    [ApiController]
+    public class AuthController : BaseController
+    {
+        private readonly IUserService _userService;
+
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost]
+        [Route(Routes.Register)]
+        public ActionResult Register([FromBody] UserRegisterDto dto)
+        {
+            string token = _userService.Register(dto);
+
+            return Ok(new { Token = token});
+        }
+    }
+}
---
+++ Server/Diplomski.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Diplomski.Presentation.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected int CurrentUserId { 
+            get => Convert.ToInt32(HttpContext?.Items["id"]); 
+        }
+    }
+}
