4d98c53e5b27ead1a058d41fedd5aa9342f19984
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-17 20:29:26 +0200

    Create validation errors consts and validate UserRegisterDto

--- Server/Diplomski.BLL/DTOs/UserRegisterDto.cs
+++ Server/Diplomski.BLL/DTOs/UserRegisterDto.cs
@@ -1,17 +1,35 @@
-﻿namespace Diplomski.BLL.DTOs
+﻿using Diplomski.BLL.Utils.Constants;
+using Diplomski.BLL.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Diplomski.BLL.DTOs
 {
     public class UserRegisterDto
     {
+        [Required]
         public string FirstName { get; set; } = null!;
+        [Required]
         public string LastName { get; set; } = null!;
+        [Required]
+        [EmailAddress(ErrorMessage = ValidationErrors.EmailInvalid)]
         public string Email { get; set; } = null!;
+        [Required]
+        [MinLength(8, ErrorMessage = ValidationErrors.PasswordMinimalLenght)]
         public string Password { get; set; } = null!;
+        [Required]
         public string ConfirmPassword { get; set; } = null!;
+        [Required]
         public string Username { get; set; } = null!;
+        [Required]
         public string PhoneNumber { get; set; } = null!;
+        [Required]
         public string Nationality { get; set; } = null!;
+        [Required]
         public bool AreTermsAndServicesAccepted { get; set; }
+        [Required]
         public bool IsPrivacyPolicyAccepted { get; set; }
+        [Required]
+        [DateTimeValidation(ErrorMessage = ValidationErrors.PastDateTime)]
         public DateTime DateOfBirth { get; set; }
     }
 }
---
+++ Server/Diplomski.BLL/Utils/Constants/ValidationErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace Diplomski.BLL.Utils.Constants
+{
+    public static class ValidationErrors
+    {
+        public const string EmailInvalid = "Email format is not valid";
+        public const string PastDateTime = "DateTime must be in past";
+        public const string PasswordMinimalLenght = "Password must have at least 8 characters";
+    }
+}
