5cbd2f56bb702f6ba3c589c43e721c9a3269dd48
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-17 10:39:41 +0200

    Add env variable and connection string

--- Server/Diplomski.Presentation/Program.cs
+++ Server/Diplomski.Presentation/Program.cs
@@ -1,8 +1,11 @@
 using Diplomski.BLL.Interfaces;
+using Diplomski.BLL.Interfaces.External;
 using Diplomski.BLL.Services;
+using Diplomski.BLL.Services.External;
 using Diplomski.DAL.Entities;
 using Diplomski.DAL.Interfaces;
 using Diplomski.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ConcurrencyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FitConDev"));
+});
 
 builder.Services.AddCors(options =>
 {
@@ -27,6 +35,10 @@ builder.Services.AddCors(options =>
 
 //Services
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+
+//ExternalServices
+builder.Services.AddScoped<ISendGridService, SendGridService>();
 
 //DbContext
 builder.Services.AddScoped<ConcurrencyDbContext>();
