1a0910bb7027a8bf97f533ddb72f3e04ebe309d1
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-25 23:31:44 +0200

    Create UserLoginDto and add validations to secret code dto

--- Server/Diplomski.BLL/DTOs/UserDtos/SecretCodeUserDto.cs
+++ Server/Diplomski.BLL/DTOs/UserDtos/SecretCodeUserDto.cs
@@ -1,6 +1,11 @@
-﻿namespace Diplomski.BLL.DTOs.UserDtos;
+﻿using System.ComponentModel.DataAnnotations;
+using Diplomski.BLL.Utils.Constants;
+
+namespace Diplomski.BLL.DTOs.UserDtos;
 
 public class SecretCodeUserDto
 {
+    [Required]
+    [StringLength(6, ErrorMessage = ValidationErrors.SecretCodeLenght)]
     public string SecretCode { get; set; } = null!;
 }
---
+++ Server/Diplomski.BLL/DTOs/UserDtos/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Diplomski.BLL.Utils.Constants;
+
+namespace Diplomski.BLL.DTOs.UserDtos;
+
+public class UserLoginDto
+{
+    [Required]
+    [EmailAddress(ErrorMessage = ValidationErrors.EmailInvalid)]
+    public string Email { get; set; } = null!;
+    
+    [Required]
+    [MinLength(8, ErrorMessage = ValidationErrors.PasswordMinimalLenght)]
+    public string Password { get; set; } = null!;
+}
