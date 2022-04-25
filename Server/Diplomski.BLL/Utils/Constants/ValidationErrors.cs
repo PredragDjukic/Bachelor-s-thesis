507ae361558bcbd466a3bef6dc445ca735361d74
djukic.predrag <predrag.djukic@marbleit.rs>
2022-04-25 23:51:05 +0200

    Add validation error for secret code lenght

--- Server/Diplomski.BLL/Utils/Constants/ValidationErrors.cs
+++ Server/Diplomski.BLL/Utils/Constants/ValidationErrors.cs
@@ -5,5 +5,6 @@
         public const string EmailInvalid = "Email format is not valid";
         public const string PastDateTime = "DateTime must be in past";
         public const string PasswordMinimalLenght = "Password must have at least 8 characters";
+        public const string SecretCodeLenght = "Secret code must have 6 characters";
     }
 }
