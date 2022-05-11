afe75aafdf1d26947b073395862cda011ba22a68
djukic.predrag <predrag.djukic@marbleit.rs>
2022-05-11 23:39:18 +0200

    Create package dtos

---
+++ Server/Diplomski.BLL/DTOs/PackageDTOs/PackageCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Diplomski.BLL.DTOs.PackageDTOs;
+
+public class PackageCreateDto
+{
+    public int NumberOfSessions { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Server/Diplomski.BLL/DTOs/PackageDTOs/PackageReadDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Diplomski.BLL.DTOs.PackageDTOs;
+
+public class PackageReadDto
+{
+    public int Id { get; set; }
+    public int NumberOfSessions { get; set; }
+    public decimal Price { get; set; }
+    public int TrainerId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+}
