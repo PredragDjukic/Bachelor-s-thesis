d58944e0dc5d075cb85a8d7376ab06dbd4940c32
djukic.predrag <predrag.djukic@marbleit.rs>
2022-05-11 23:23:26 +0200

    Create PackageRepository and implement Create

---
+++ Server/Diplomski.DAL/Interfaces/IPackageRepository.cs
@@ -0,0 +1,8 @@
+﻿using Diplomski.DAL.Entities;
+
+namespace Diplomski.DAL.Interfaces;
+
+public interface IPackageRepository
+{
+    void Create(Package entity);
+}
---
+++ Server/Diplomski.DAL/Repositories/PackageRepository.cs
@@ -0,0 +1,26 @@
+﻿using Diplomski.DAL.Entities;
+using Diplomski.DAL.Interfaces;
+
+namespace Diplomski.DAL.Repositories;
+
+public class PackageRepository : IPackageRepository
+{
+    private readonly ConcurrencyDbContext _context;
+
+    
+    public PackageRepository(ConcurrencyDbContext context)
+    {
+        _context = context;
+    }
+
+    
+    public void Create(Package entity)
+    {   
+        entity.CreatedAt = DateTime.UtcNow;
+        entity.UpdatedAt = DateTime.UtcNow;
+
+        _context.Package.Add(entity);
+
+        _context.SaveChanges();
+    }
+}
