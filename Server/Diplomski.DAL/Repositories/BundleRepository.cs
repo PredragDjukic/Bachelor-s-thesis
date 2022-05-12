45f52df8db8d345b09bdab4fbef272c6fbfa8c92
djukic.predrag <predrag.djukic@marbleit.rs>
2022-05-12 22:11:09 +0200

    Create bundle repository and implement bundle creation

---
+++ Server/Diplomski.DAL/Repositories/BundleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Diplomski.DAL.Entities;
+using Diplomski.DAL.Interfaces;
+
+namespace Diplomski.DAL.Repositories;
+
+public class BundleRepository : IBundleRepository
+{
+    private readonly ConcurrencyDbContext _context;
+
+    
+    public BundleRepository(ConcurrencyDbContext context)
+    {
+        _context = context;
+    }
+    
+
+    public Bundle Create(Bundle entity)
+    {
+        entity.CreatedAt = DateTime.UtcNow;
+        entity.UpdatedAt = DateTime.UtcNow;
+
+        _context.Bundle.Add(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+}
